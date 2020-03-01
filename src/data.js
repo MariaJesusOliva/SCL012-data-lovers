3dfd1ea70d66d6d8406bf30006d44aa5332283a5
MariaJesusOliva <kexu.olivap@gmail.com>
2020-03-01 02:45:20 -0300

    funcionalidades DOM I

--- src/data.js
+++ src/data.js
@@ -5,3 +5,18 @@
 export const example = () => {
   return 'example';
 };
+//show options to order names
+const orderNames = document.getElementsById("submenu");
+orderNames.addEventListener("onmouseover", function() {
+  
+})
+//show more information in the welcome by the seemore button
+const seeMore = document.getElementById("seeMoreButton");
+seeMore.addEventListener("click", function() {
+  document.getElementsById('seeMore').style.display="block"
+})
+//hide more information in the welcome by the seeless button
+const seeLess = document.getElementsById("seeLessButton");
+seeLess.addEventListener("click", function() {
+  document.getElementsById('seeMore').style.display="none"
+})
