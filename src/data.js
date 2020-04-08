56a511300896859d8370cfc6453232bbe6131250
MariaJesusOliva <kexu.olivap@gmail.com>
2020-04-08 08:36:43 -0400

    Principal Page

--- src/data.js
+++ src/data.js
@@ -6,17 +6,20 @@ export const example = () => {
   return 'example';
 };
 //show options to order names
-const orderNames = document.getElementsById("submenu");
-orderNames.addEventListener("onmouseover", function() {
+const showSubmenu = document.getElementsById("submenu");
+//add event on click
+showSubmenu.addEventListener("click", function() {
+  document.getElementsById('submenu-menu').style.display = "block";
   
 })
+/*
 //show more information in the welcome by the seemore button
 const seeMore = document.getElementById("seeMoreButton");
 seeMore.addEventListener("click", function() {
-  document.getElementsById('seeMore').style.display="block"
+  document.getElementsById('seeMore').style.display = "block";
 })
 //hide more information in the welcome by the seeless button
 const seeLess = document.getElementsById("seeLessButton");
 seeLess.addEventListener("click", function() {
   document.getElementsById('seeMore').style.display="none"
-})
+}) */
