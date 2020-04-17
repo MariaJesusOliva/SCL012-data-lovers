5c975eaa075a9393c657c06dd6bd4d770bcb642c
MariaJesusOliva <kexu.olivap@gmail.com>
2020-04-17 13:11:41 -0400

    add div and function to seeMore Button

--- src/data.js
+++ src/data.js
@@ -2,16 +2,9 @@
 
 // esta es una función de ejemplo
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
-//show options to order names
-const showSubmenu = document.getElementsById("submenu");
-//add event on click
-showSubmenu.addEventListener("click", function() {
-  document.getElementsById('submenu-menu').style.display = "block";
-  
-})
 /*
 //show more information in the welcome by the seemore button
 const seeMore = document.getElementById("seeMoreButton");
--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
-import POTTER from './data/potter/potter.js'
+import POTTER from './data/potter/potter.js';
 
-console.log(POTTER)
+console.log(POTTER);
+
+//SHOW MORE INFO IN WELCOME
+//create div
+let addFirstElement = document.createElement('div');
+let elementForTrailer = document.createElement('h3');
+let textForTrailer = document.createTextNode('Aqui puedes ver el tráiler en español');
+let addLinkTrailer = document.createElement('iframe');
+let addButton = document.createElement('button');
+let addTextButton = document.createTextNode('Ver menos');
+addFirstElement.className = 'seeMore content';
+addFirstElement.id = 'seeMore';
+addLinkTrailer.className = 'youtubeLink'
+addLinkTrailer.src = '"https://www.youtube.com/embed/3cA_EAtkaLc';
+addButton.className = 'seeLess button';
+addButton.id = 'seeLess';
+addFirstElement.appendChild(elementForTrailer);
+addFirstElement.appendChild(addLinkTrailer);
+addFirstElement.appendChild(addButton);
+elementForTrailer.appendChild(textForTrailer);
+addButton.appendChild(addTextButton);
+
+//function to show div
+function seeMoreInfo(){
+    console.log(addFirstElement);
+}
+//function in button
+document.getElementById('seeMoreButton').onclick = function (){
+    seeMoreInfo();
+}
